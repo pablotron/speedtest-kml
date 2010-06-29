ad2f834070e45fed793b1804a06c3b93d86e5da6
pabs <pabs@pablotron.org>
2010-06-28 23:06:32 -0400

    add namedconverter

--- web/speedtest-web.rb
+++ web/speedtest-web.rb
@@ -8,14 +8,55 @@ module SpeedTest
     HTML = %(
       <html>
         <head>
-          <title>SpeedTest CSV to KML</title>
+          <title>SpeedTest KML Generator</title>
+          <style type='text/css'>
+            body { 
+              background-color: white;
+              font-family: verdana, helvetica, arial, sans-serif;
+              font-size: 9pt;
+            }
+
+            div.title {
+              font-size: 12pt;
+              font-weight: bold;
+              margin-bottom: 10px;
+            }
+
+            td.label {
+              text-align: right;
+              font-size:  9pt;
+              color: #333;
+            }
+          </style>
         </head>
 
         <body>
-          <form method='post'>
-            <label for='csv'>CSV:</label>
-            <input type='file' id='csv' name='csv'/>
-            <input type='submit' value='Convert'/>
+          <div class='title'>
+            SpeedTest KML Generator
+          </div>
+
+          <form method='post' enctype='multipart/form-data'>
+            <table>
+              <tr>
+                <td class='label'><label for='name'>Name:</label></td>
+                <td><input type='text' id='name' name='name'/></td>
+              </tr>
+
+              <tr>
+                <td class='label'><label for='desc'>Description:</label></td>
+                <td><input type='text' id='desc' name='desc'/></td>
+              </tr>
+
+              <tr>
+                <td class='label'><label for='csv'>CSV (required):</label></td>
+                <td><input type='file' id='csv' name='csv'/></td>
+              </tr>
+
+              <tr>
+                <td colspan='2'>
+                  <input type='submit' value='Create KML'/>
+                </td>
+              </tr>
           </form>
         </body>
       </html>
@@ -28,19 +69,17 @@ module SpeedTest
     end
 
     post '/' do
-      req = Rack::Request.new(env)
-      data = req.params['csv'][:tempfile]
+      # get name and data
+      name = params[:csv][:name].gsub(/\.[^.]+$/, '') + '.kml'
+      data = params[:csv][:tempfile].read
 
-      status 200
-      content_type 'text/html'
-      body [data]
-# 
-#       return [200
-# 
-#       attachment   'speedtest.kml'
-#       content_type 'application/vnd.google-earth.kml+xml'
-#       body         SpeedTest::Converter.run(data)
-# 
+      attachment   name
+      content_type 'application/vnd.google-earth.kml+xml'
+      body         SpeedTest::Converter.run(
+        params[:name] || '',
+        params[:desc] || '',
+        params[:csv][:tempfile].read
+      )
     end
   end
 end
